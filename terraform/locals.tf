0a9af40a346e7b3040820ddad3d7d98018796029
getglad <1sembyl8p@mozmail.com>
2023-05-20 17:59:50 -0400

    null

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,5 @@
 resource "random_pet" "project" {}
 
 locals {
-  project_name = var.project_name ? var.project_name : random_pet.project.keepers.name
+  project_name = var.project_name == null ? var.project_name : random_pet.project.keepers.name
 }
