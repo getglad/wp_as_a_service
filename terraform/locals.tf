06f58d41f8fb76212830384c2caeafd20dbc522c
getglad <1sembyl8p@mozmail.com>
2023-05-20 18:10:09 -0400

    random

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,5 @@
 resource "random_pet" "project" {}
 
 locals {
-  project_name = var.project_name == null ? var.project_name : random_pet.project.keepers.id
+  project_name = var.project_name == null ? var.project_name : random_pet.project.id
 }
--- terraform/storage/spaces.tf
+++ terraform/storage/spaces.tf
@@ -1,7 +1,7 @@
 resource "random_pet" "project" {}
 
 resource "digitalocean_spaces_bucket" "wpaas" {
-  name   = random_pet.project.keepers.id
+  name   = random_pet.project.id
   region = "nyc3"
 
   versioning {
