50328ffc5d08ab0c8971b465066ff85247983b0b
getglad <1sembyl8p@mozmail.com>
2023-05-20 17:45:32 -0400

    swap provider

--- terraform/compute/main.tf
+++ terraform/compute/main.tf
@@ -1,1 +1,13 @@
-provider "digitalocean" {}
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.28"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.5"
+    }
+  }
+}
