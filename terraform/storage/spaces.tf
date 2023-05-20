1a457698048c84ec1d6be052b0dab8c7d7c57653
getglad <1sembyl8p@mozmail.com>
2023-05-20 18:03:36 -0400

    get id of random

--- terraform/storage/spaces.tf
+++ terraform/storage/spaces.tf
@@ -1,7 +1,7 @@
 resource "random_pet" "project" {}
 
 resource "digitalocean_spaces_bucket" "wpaas" {
-  name   = random_pet.project.keepers.name
+  name   = random_pet.project.keepers.name.id
   region = "nyc3"
 
   versioning {
